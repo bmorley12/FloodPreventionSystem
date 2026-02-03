1f431c646c0d252408d746f5c7414ff925b98504
Ben Morley <bmorley12@protonmail.com>
2026-02-03 09:33:37 -0700

    basic wifi config

--- FloodPreventionSystem/src/main.cpp
+++ FloodPreventionSystem/src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Prototypes
+void wifi_connect();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  delay(1000);
+
+  wifi_connect();
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Basic 2.4GHz WiFi connection
+void wifi_connect() {
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
